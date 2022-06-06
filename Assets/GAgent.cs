e89c84d48b5326265d7e9b6a9fe0ebc93fb96d99
mopkaloppt <moppi.sgotmoney@gmail.com>
2022-06-06 21:59:18 +0200

    Add GAgent(Unfinished)
    End of Lesson 13.

--- Assets/GAgent.cs
+++ Assets/GAgent.cs
@@ -1,17 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq; // For Sorting Goals
 
+public class SubGoal
+{
+    public Dictionary<string, int> subgoals;
+    public bool remove; // once goal is satisfied, it is removed
+
+    public SubGoal(string s, int i, bool r)
+    {
+        subgoals = new Dictionary<string, int>();
+        subgoals.Add(s, i);
+        remove = r;
+    }
+}
 public class GAgent : MonoBehaviour
 {
+    public List<GAction> actions = new List<GAction>();
+    public Dictionary<SubGoal, int> goals = new Dictionary<SubGoal, int>();
+
+    GPlanner planner;
+    Queue<GAction> actionQueue;
+    public GAction currentAction;
+    SubGoal currentGoal;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GAction[] acts = this.GetComponents<GAction>();
+        foreach (GAction a in acts)
+            actions.Add(a);     
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         
     }
